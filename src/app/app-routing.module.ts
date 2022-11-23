a3d7e2bca91db834d88a8e7ebef0be51909ecd09
Agnieszka Bagińska <baginskaagnieszka6@gmail.com>
2022-11-23 23:19:41 +0100

    feat: Create select box with options and list displayed under public-holidays path in url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,27 @@ import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { CryptoChipsListComponent } from './components/crypto-chips-list/crypto-chips-list.component';
+import { SelectBoxComponent } from './components/select-box/select-box.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategoryListComponentModule } from './components/category-list/category-list.component-module';
 import { CategoryServiceModule } from './services/category.service-module';
 import { CryptoChipsListComponentModule } from './components/crypto-chips-list/crypto-chips-list.component-module';
 import { CryptoChipsServiceModule } from './services/crypto-chips.service-module';
+import { SelectBoxComponentModule } from './components/select-box/select-box.component-module';
+import { SelectBoxServiceModule } from './services/select-box.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
     { path: 'products', component: ProductListComponent },
     { path: 'category-list', component: CategoryListComponent },
-    { path: 'CryptoChips', component: CryptoChipsListComponent }
+    { path: 'CryptoChips', component: CryptoChipsListComponent },
+    { path: 'public-holidays', component: SelectBoxComponent }
   ]),
     ProductListComponentModule, ProductsServiceModule,
     CategoryListComponentModule, CategoryServiceModule,
-    CryptoChipsListComponentModule, CryptoChipsServiceModule],
+    CryptoChipsListComponentModule, CryptoChipsServiceModule,
+    SelectBoxComponentModule, SelectBoxServiceModule],
   exports: [RouterModule]
 })
 
---
+++ src/app/components/select-box/select-box.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { SelectBoxService } from '../../services/select-box.service';
+import {Observable} from "rxjs";
+import {SelectBoxModel} from "../../models/select-box.model";
+
+@Component({
+  selector: 'app-select-box',
+  styleUrls: ['./select-box.component.scss'],
+  templateUrl: './select-box.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SelectBoxComponent {
+  readonly list$: Observable<SelectBoxModel[]> = this._selectBoxService.getAll();
+  constructor(private _selectBoxService: SelectBoxService) {
+  }
+}
